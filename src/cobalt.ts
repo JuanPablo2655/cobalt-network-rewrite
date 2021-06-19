8fe4ef88027373bd33f0a7d281a56236d8e898e9
JuanPablo2655 <personal@isidrocasiano.com>
2021-06-19 00:18:52 -0400

    added option packages for faster ws connection and added guildCreate and guildDelete both untested

--- src/cobalt.ts
+++ src/cobalt.ts
@@ -3,7 +3,7 @@ import './utils/mongo';
 const cobalt: CobaltClient = new CobaltClient();
 
 if (cobalt.devMode) {
-	cobalt.on('debug', console.log);
+	cobalt.on('debug', console.log).on('warn', console.log);
 }
 
 cobalt.start();
---
+++ src/events/guild/guildDelete.ts
@@ -0,0 +1,30 @@
+import { Guild, MessageEmbed, Snowflake, WebhookClient } from "discord.js";
+import Event from "../../struct/Event";
+
+abstract class GuildDeleteEvent extends Event {
+    constructor() {
+        super({
+            name: "guildDelete"
+        });
+    };
+
+    async run(guild: Guild) {
+        if (!this.cobalt.testEvents) return;
+        if (!guild) return
+        if (!guild.available) return;
+        await this.cobalt.db.removeGuild(guild.id);
+        const cobaltHook = new WebhookClient(
+			'841886640682958909' as Snowflake,
+			'Ncp5ATyT9qvZTXPfNxlQ9L6Si-Sfp2BljzbCUrleoAIuBAtIyP1EORJefEXMmCkU79XS',
+		);
+		const guildEmbed = new MessageEmbed()
+			.setTitle(`Guild Deleted`)
+            .setThumbnail(guild.iconURL({ format: "png", dynamic: true }) ?? "")
+			.setDescription(`Guild Name: **${guild.name}**\nGuild ID: **${guild.id}**\nmember: **${guild.memberCount}**`)
+            .setFooter(`Now I'm in ${this.cobalt.guilds.cache.size} guilds`)
+			.setTimestamp();
+		cobaltHook.send({ embeds: [guildEmbed] });
+    };
+};
+
+export default GuildDeleteEvent;
