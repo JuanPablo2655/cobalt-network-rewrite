ff04ae5c5d95cedf8f3e6c5afb15e13b09303c99
JuanPablo2655 <personal@isidrocasiano.com>
2021-07-02 15:36:04 -0400

    feat: added copper and steel sword as well carbonite and steel ingots

--- src/data/items/weapons.ts
+++ src/data/items/weapons.ts
@@ -1,8 +1,8 @@
 import { ItemData } from './items';
 
 export interface WeaponData extends ItemData {
-	damage: number;
 	isWeapon: boolean;
+	damage: number;
 }
 
 const weapons: WeaponData[] = [
@@ -17,8 +17,36 @@ const weapons: WeaponData[] = [
 		price: 50,
 		sellAmount: 20,
 		keep: true,
-		damage: 20,
 		isWeapon: true,
+		damage: 5,
+	},
+	{
+		id: 'ironsword',
+		name: 'Iron Sword',
+		category: 'weapons',
+		description: "At least it's an upgrade from the copper sword.",
+		craftable: true,
+		canUse: true,
+		canBuy: true,
+		price: 110,
+		sellAmount: 45,
+		keep: true,
+		isWeapon: true,
+		damage: 8,
+	},
+	{
+		id: 'steelsword',
+		name: 'Steel Sword',
+		category: 'weapons',
+		description: "Now we're talking the most common weapon out there.",
+		craftable: true,
+		canUse: true,
+		canBuy: true,
+		price: 50,
+		sellAmount: 20,
+		keep: true,
+		isWeapon: true,
+		damage: 15,
 	},
 ];
 
