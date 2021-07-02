8cd1db9f3d3c058b698e2476e7e75f31ef1fdf07
JuanPablo2655 <personal@isidrocasiano.com>
2021-07-02 15:09:34 -0400

    feat: first weapon

--- src/data/items/weapons.ts
+++ src/data/items/weapons.ts
@@ -1,10 +1,25 @@
-import { ItemCategories, ItemData } from './items';
+import { ItemData } from './items';
 
 export interface WeaponData extends ItemData {
-	id: string;
-	name: string;
-	category: ItemCategories;
-	description: string;
-	canBuy: boolean;
 	damage: number;
+	isWeapon: boolean;
 }
+
+const weapons: WeaponData[] = [
+	{
+		id: 'coppersword',
+		name: 'Copper Sword',
+		category: 'weapons',
+		description: 'The most basic weapon for defense or offense.',
+		craftable: true,
+		canUse: true,
+		canBuy: true,
+		price: 50,
+		sellAmount: 20,
+		keep: true,
+		damage: 20,
+		isWeapon: true,
+	},
+];
+
+export default weapons;
