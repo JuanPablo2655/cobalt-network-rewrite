70e5064739f7f64706465c560d840343bef8c07e
JuanPablo2655 <personal@isidrocasiano.com>
2021-07-05 23:04:14 -0400

    feat: potions

--- src/data/items/potions.ts
+++ src/data/items/potions.ts
@@ -3,6 +3,7 @@ import { ItemData, Item } from './items';
 declare enum PotionStatType {
 	heal = 0,
 	magicka = 1,
+	all = 2,
 }
 
 declare enum PotionEffectType {
@@ -24,6 +25,49 @@ export class Potion extends ItemData {
 	}
 }
 
-const potions: Item[] = [];
+const potions: Item[] = [
+	{
+		id: 'health',
+		name: 'Potion of Health',
+		description: 'Health is increased by 20 points for 60 seconds.',
+		data: new Potion(0, 20, 2),
+	},
+	{
+		id: 'magicka',
+		name: 'Potion of Extra Magicka',
+		description: 'Magicka is increase by 20 points for 60 seconds.',
+		data: new Potion(1, 20, 2),
+	},
+	{
+		id: 'health1',
+		name: 'Potion of Regeneration',
+		description: 'Health regenerates 50% faster for 60 seconds.',
+		data: new Potion(0, 50, 1),
+	},
+	{
+		id: 'magicka1',
+		name: 'Potion of Lasting Potency',
+		description: 'Magicka regenerates 50% faster for 60 seconds.',
+		data: new Potion(1, 50, 1),
+	},
+	{
+		id: 'health2',
+		name: 'Potion of Healing',
+		description: 'Restore 30 points of Health.',
+		data: new Potion(0, 30, 0),
+	},
+	{
+		id: 'magicka2',
+		name: 'Potion of Magicka',
+		description: 'Restore 30 points of Magicka.',
+		data: new Potion(1, 30, 0),
+	},
+	{
+		id: 'magicka2',
+		name: 'Potion of Well-being',
+		description: 'Restore 20 points of Health. Restore 20 points of Magicka.',
+		data: new Potion(1, 20, 0),
+	},
+];
 
 export default potions;
