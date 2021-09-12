a5df90ec75ccdf90fe5ad50ff1719b7970f34578
JuanPablo2655 <personal@isidrocasiano.com>
2021-09-11 23:37:03 -0400

    test: most of util

--- tests/utils/util.test.ts
+++ tests/utils/util.test.ts
@@ -1,4 +1,4 @@
-import { formatMoney, formatNumber, toCapitalize } from '../src/utils/util';
+import { addMulti, formatMoney, formatNumber, getDiff, toCapitalize, trim } from '../../src/utils/util';
 
 describe('Cobalt', () => {
 	describe('util', () => {
@@ -41,6 +41,42 @@ describe('Cobalt', () => {
 
 			test("GIVEN '1244' THEN return '1244'", () => {
 				expect(toCapitalize('1244')).toEqual('1244');
+			});
+		});
+
+		describe('findMember', () => {
+			test.todo('GIVEN client AND Message AND args AND options THEN return User');
+		});
+
+		describe('findRole', () => {
+			test.todo('GIVEN Message AND arg THEN return Role');
+		});
+
+		describe('findChannel', () => {
+			test.todo('GIVEN Message AND arg THEN return TextChannel');
+		});
+
+		describe('trim', () => {
+			test("GIVEN 'This string is going to be trimmed.' AND 14 THEN return 'This string...'", () => {
+				expect(trim('This string is going to be trimmed.', 14)).toEqual('This string...');
+			});
+
+			test("GIVEN 'This string is going to be trimmed.' AND 14 THEN return 'This string is going to be trimmed.'", () => {
+				expect(trim('This string is going to be trimmed.', 100)).toEqual('This string is going to be trimmed.');
+			});
+		});
+
+		describe('getDiff', () => {
+			test("GIVEN 'This is the first string' AND 'This is the second string' THEN return 'This is the ~~first~~***second*** string'", () => {
+				expect(getDiff('This is the first string', 'This is the second string')).toEqual(
+					'This is the ~~first~~***second*** string',
+				);
+			});
+		});
+
+		describe('addMulti', () => {
+			test('GIVEN 100 AND 10 THEN return 110', () => {
+				expect(addMulti(100, 10)).toEqual(110);
 			});
 		});
 	});
