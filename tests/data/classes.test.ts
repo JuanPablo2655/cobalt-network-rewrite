87228302b7d5433aba8a55648282f15b1b417bea
Isidro Casiano <isidroICP@outlook.com>
2021-09-13 15:52:59 -0400

    feat: jobs and class test

---
+++ tests/data/classes.test.ts
@@ -0,0 +1,3 @@
+describe('classes', () => {
+    test.todo('GIVEN index of 1 THEN return something idk')
+});
---
+++ tests/data/jobs.test.ts
@@ -0,0 +1,13 @@
+import { jobs } from 'src/data/jobs'
+
+describe('classes', () => {
+    test("GIVEN 'developer' THEN return developer object", () => {
+        expect(jobs.find(j => j === 'developer')).toEqual({
+            id: 'developer',
+            name: 'Developer',
+            entries: ['**{user.username}** developed a program for a client and got paid **₡{money}**.'],
+            firedEntry: '**{user.username}**  got fired for missing too many deadlines.',
+            minAmount: 500,
+        });
+    })
+});
