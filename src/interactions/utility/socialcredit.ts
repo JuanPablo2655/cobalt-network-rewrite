b9b239c7e2c091f0de2f3af1f8356e361ade132c
JuanPablo2655 <personal@isidrocasiano.com>
2021-10-23 00:43:46 -0400

    feat: social credit command added

--- src/interactions/utility/socialcredit.ts
+++ src/interactions/utility/socialcredit.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction } from 'discord.js';
 import Interaction from '../../struct/InteractionCommand';
 import { socialcreditCommand } from './options';
+import { add, check, remove } from './subcommands';
 
 abstract class SocialCreditInteraction extends Interaction {
 	constructor() {
@@ -10,7 +11,25 @@ abstract class SocialCreditInteraction extends Interaction {
 		});
 	}
 
-	async run(interaction: CommandInteraction) {}
+	async run(interaction: CommandInteraction) {
+		const command = interaction.options.getSubcommand(true);
+		switch (command) {
+			case 'check': {
+				await check(this.cobalt, interaction);
+				break;
+			}
+			case 'add': {
+				await add(this.cobalt, interaction);
+				break;
+			}
+			case 'remove': {
+				await remove(this.cobalt, interaction);
+				break;
+			}
+			default:
+				break;
+		}
+	}
 }
 
 export default SocialCreditInteraction;
