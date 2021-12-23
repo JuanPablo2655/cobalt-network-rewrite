10ee87c6b4b1f002c5af478707be9a8b0e2264ac
JuanPablo2655 <personal@isidrocasiano.com>
2021-12-22 21:28:14 -0500

    feat: UserError Options

--- src/lib/errors/UserError.ts
+++ src/lib/errors/UserError.ts
@@ -1,9 +1,17 @@
 export class UserError extends Error {
-	public constructor(message: string) {
+	public readonly context: unknown;
+	public constructor(options: UserError.Options, message: string) {
 		super(message);
+		this.context = options.context ?? null;
 	}
 
 	public get name(): string {
 		return 'UserError';
+	}
+}
+
+export namespace UserError {
+	export interface Options {
+		context?: unknown;
 	}
 }
