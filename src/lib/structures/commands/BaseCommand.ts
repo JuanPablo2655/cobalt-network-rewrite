384b8d69735d563c022f40c14ca1cbe16a850395
Isidro Casiano <personal@isidrocasiano.com>
2022-01-23 22:33:59 -0500

    feat: add more documentation to lib

--- src/lib/structures/commands/BaseCommand.ts
+++ src/lib/structures/commands/BaseCommand.ts
@@ -3,12 +3,37 @@ import { BaseCommandOptions, Categories } from '../../typings/CommandOptions';
 import { CobaltClient } from '../../cobaltClient';
 
 export abstract class BaseCommand {
+	/**
+	 * The name for the command
+	 */
 	public name: string;
+
+	/**
+	 * Category in which the command belongs to
+	 */
 	public category: Categories;
+
+	/**
+	 * Option to make it developer only
+	 */
 	public devOnly: boolean;
+
+	/**
+	 * The permissions the user needs to run the command
+	 */
 	public userPermissions: PermissionString[];
+
+	/**
+	 * The permission the client needs to properly run
+	 */
 	public clientPermissions: PermissionString[];
+
 	public abstract cobalt: CobaltClient;
+
+	/**
+	 * Constructs a BaseCommand
+	 * @param options Optional Command settings other than name and category
+	 */
 	constructor(options: BaseCommandOptions) {
 		this.name = options.name;
 		this.category = options.category;
--- src/lib/structures/commands/GenericCommand.ts
+++ src/lib/structures/commands/GenericCommand.ts
@@ -1,17 +1,55 @@
+import { seconds } from '#utils/common';
 import { Message } from 'discord.js';
 import { GenericCommandType } from '../../typings/CommandOptions';
 import { BaseCommand } from './BaseCommand';
 
 export abstract class GenericCommand extends BaseCommand {
+	/**
+	 * The description for the command
+	 */
 	public description: string;
+
+	/**
+	 * description on how to use the command
+	 */
 	public usage: string;
+
+	/**
+	 * The alias used to run this command
+	 */
 	public aliases: string[];
+
+	/**
+	 * Whether the command is enabled gloablly or not
+	 */
 	public enabled: boolean;
+
+	/**
+	 * Whether the command is owner only or not
+	 */
 	public ownerOnly: boolean;
+
+	/**
+	 * Whether the command is guild only or not
+	 */
 	public guildOnly: boolean;
+
+	/**
+	 * Whether the command is nsfw
+	 *
+	 * The command only works in channels marked as nsfw
+	 */
 	public nsfwOnly: boolean;
+
+	/**
+	 * How long the cooldown is in milliseconds
+	 */
 	public cooldown: number;
 
+	/**
+	 * Constructs a GenericCommand
+	 * @param options Optional Command settings other than name, category, and description
+	 */
 	constructor(options: GenericCommandType) {
 		super(options);
 		this.description = options.description;
@@ -21,10 +59,16 @@ export abstract class GenericCommand extends BaseCommand {
 		this.ownerOnly = options.ownerOnly ?? false;
 		this.guildOnly = options.guildOnly ?? false;
 		this.nsfwOnly = options.nsfwOnly ?? false;
-		this.cooldown = options.cooldown ?? 1;
+		this.cooldown = options.cooldown ?? seconds(1);
 		this.userPermissions = options.userPermissions ?? ['SEND_MESSAGES'];
 		this.clientPermissions = options.clientPermissions ?? ['SEND_MESSAGES', 'READ_MESSAGE_HISTORY', 'EMBED_LINKS'];
 	}
 
+	/**
+	 * Executes the message command's logic
+	 * @param message The message that triggered the command
+	 * @param args The parsed arugments
+	 * @param addCD Adds cooldown to the user
+	 */
 	public abstract run(message: Message, args: string[], addCD: () => Promise<void>): unknown | Promise<unknown>;
 }
