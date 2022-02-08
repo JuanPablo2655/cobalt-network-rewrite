c1252051a7ea33e48abffa0a8f55eb1457f245e9
Isidro Casiano <personal@isidrocasiano.com>
2022-02-08 02:16:47 -0500

    feat: moved removeDuplicates to utils and refactored getImage

--- src/commands/settings/disablecommand.ts
+++ src/commands/settings/disablecommand.ts
@@ -1,6 +1,7 @@
 import { Guild, Message } from 'discord.js';
 import { GenericCommand } from '#lib/structures/commands';
 import { Identifiers, UserError } from '#lib/errors';
+import { SAVE_CATEGORIES, SAVE_COMMANDS } from '#utils/constants';
 
 abstract class DisableCommandCommand extends GenericCommand {
 	constructor() {
@@ -14,9 +15,6 @@ abstract class DisableCommandCommand extends GenericCommand {
 	}
 
 	async run(message: Message, args: string[], addCD: () => Promise<void>) {
-		// TODO(Isidro): put this into a constant file
-		const saveCommands = ['help', 'enablecommand', 'disablecommand'];
-		const saveCategories = ['dev', 'settings'];
 		if (!args[0]) throw new UserError({ identifer: Identifiers.ArgsMissing }, 'Missing command');
 		const arg = args[0].toLowerCase();
 		const command = this.cobalt.commands.get(arg);
@@ -24,9 +22,9 @@ abstract class DisableCommandCommand extends GenericCommand {
 		const guild = await this.cobalt.db.getGuild(guildId);
 		if (!guild) throw new Error('Missing guid database entry');
 		if (!command) throw new UserError({ identifer: Identifiers.PreconditionMissingData }, 'Invalid command');
-		if (saveCommands.includes(command.name))
+		if (SAVE_COMMANDS.includes(command.name))
 			throw new UserError({ identifer: Identifiers.CommandDisabled }, `Can't disable \`${command}\``);
-		if (saveCategories.includes(command?.category))
+		if (SAVE_CATEGORIES.includes(command?.category))
 			throw new UserError(
 				{ identifer: Identifiers.CategoryDisabled },
 				`Can't disabled command in \`${command.category}\` category`,
