248757aa84a64922b88f18d32952812ba27a3829
Isidro Casiano <personal@isidrocasiano.com>
2022-02-09 01:18:04 -0500

    refactor: items now uses the enums

--- src/lib/data/items/potions.ts
+++ src/lib/data/items/potions.ts
@@ -1,15 +1,15 @@
 import { ItemData, Item, Market } from './items';
 
 declare enum PotionStatType {
-	heal = 0,
-	magicka = 1,
-	all = 2,
+	Heal = 0,
+	Magicka = 1,
+	All = 2,
 }
 
 declare enum PotionEffectType {
-	once = 0,
-	lasting = 1,
-	buff = 2,
+	Once = 0,
+	Lasting = 1,
+	Buff = 2,
 }
 
 export class Potion extends ItemData {
@@ -30,43 +30,43 @@ const potions: Item[] = [
 		id: 'health',
 		name: 'Potion of Health',
 		description: 'Health is increased by 20 points for 60 seconds.',
-		data: new Potion(0, 20, 2).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Heal, 20, PotionEffectType.Buff).join(new Market(40, 16)),
 	},
 	{
 		id: 'magicka',
 		name: 'Potion of Extra Magicka',
 		description: 'Magicka is increase by 20 points for 60 seconds.',
-		data: new Potion(1, 20, 2).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Magicka, 20, PotionEffectType.Buff).join(new Market(40, 16)),
 	},
 	{
 		id: 'health1',
 		name: 'Potion of Regeneration',
 		description: 'Health regenerates 50% faster for 60 seconds.',
-		data: new Potion(0, 50, 1).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Heal, 50, PotionEffectType.Lasting).join(new Market(40, 16)),
 	},
 	{
 		id: 'magicka1',
 		name: 'Potion of Lasting Potency',
 		description: 'Magicka regenerates 50% faster for 60 seconds.',
-		data: new Potion(1, 50, 1).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Magicka, 50, PotionEffectType.Lasting).join(new Market(40, 16)),
 	},
 	{
 		id: 'health2',
 		name: 'Potion of Healing',
 		description: 'Restore 30 points of Health.',
-		data: new Potion(0, 30, 0).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Heal, 30, PotionEffectType.Once).join(new Market(40, 16)),
 	},
 	{
 		id: 'magicka2',
 		name: 'Potion of Magicka',
 		description: 'Restore 30 points of Magicka.',
-		data: new Potion(1, 30, 0).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Magicka, 30, PotionEffectType.Once).join(new Market(40, 16)),
 	},
 	{
 		id: 'magicka2',
 		name: 'Potion of Well-being',
 		description: 'Restore 20 points of Health. Restore 20 points of Magicka.',
-		data: new Potion(1, 20, 0).join(new Market(40, 16)),
+		data: new Potion(PotionStatType.Magicka, 20, PotionEffectType.Once).join(new Market(40, 16)),
 	},
 ];
 
