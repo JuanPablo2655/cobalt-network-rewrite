f0aa6edb23a6d5494d6633b3c76ec9760754cb7f
JuanPablo2655 <personal@isidrocasiano.com>
2022-12-20 16:31:32 -0500

    docs: update truncate

--- src/lib/utils/functions/strings.ts
+++ src/lib/utils/functions/strings.ts
@@ -10,9 +10,10 @@ export function toCapitalize(str: string) {
 }
 
 /**
- * Trim a string to a certain length
- * @param str The string to trim
+ * Truncate a string to a certain length
+ * @param str The string to truncate
  * @param max The max length of the string
+ * @returns The truncated string minus the last 3 characters
  */
 export function truncate(str: string, max: number) {
 	return str.length > max ? `${str.slice(0, max - 3)}...` : str;
